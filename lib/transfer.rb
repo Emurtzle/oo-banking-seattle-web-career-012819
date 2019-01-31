fee0b0c9305476ef8b7a7fcee8d4d81d6a91ca07
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 19:52:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @status == pending
+    if @status == "pending"
       @sender.balance = @sender.balance - amount
       @receiver.deposit(amount)
       @status = "complete"
