fdc044ca35b63ac5764f52e16e61713b1e68c94f
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 19:56:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,4 +24,12 @@ class Transfer
       end
     end
   end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance = @sender.balance + amount
+      @receiver.balance = @receiver.balance - amount
+      @status = "reversed"
+    end
+  end
 end
